9a59653e2ba05e1a805e17ae923e3037f4ddc81b
ricksy <ahmed@mailbox.org>
2024-10-15 22:56:26 +0200

    working

--- app.py
+++ app.py
@@ -1,33 +1,57 @@
-import asyncssh
-import asyncio
+#!/usr/bin/env python3.7
+#
+# Copyright (c) 2017-2024 by Ron Frederick <ronf@timeheart.net> and others.
+#
+# This program and the accompanying materials are made available under
+# the terms of the Eclipse Public License v2.0 which accompanies this
+# distribution and is available at:
+#
+#     http://www.eclipse.org/legal/epl-2.0/
+#
+# This program may also be made available under the following secondary
+# licenses when the conditions for such availability set forth in the
+# Eclipse Public License v2.0 are satisfied:
+#
+#    GNU General Public License, Version 2.0, or any later versions of
+#    that license
+#
+# SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
+#
+# Contributors:
+#     Ron Frederick - initial implementation, API, and documentation
 
-class MySSHServer(asyncssh.SSHServer):
-    def connection_made(self, conn):
-        print('Connection made to SSH server.')
+# To run this program, the file ``ssh_host_key`` must exist with an SSH
+# private key in it to use as a server host key. An SSH host certificate
+# can optionally be provided in the file ``ssh_host_key-cert.pub``.
+#
+# The file ``ssh_user_ca`` must exist with a cert-authority entry of
+# the certificate authority which can sign valid client certificates.
 
-    def session_requested(self):
-        return MySSHServerSession()
+import asyncio, asyncssh, subprocess, sys
 
-    def begin_auth(self, username):
-        # Disable authentication
-        return False
+async def handle_client(process: asyncssh.SSHServerProcess) -> None:
+    #cmd ="sleep 5"
+    #cmd = "which python3"
+    cmd = "python3 splash.py"
+    #cmd = "python3 hello_world.py"
+    bc_proc = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE,
+                               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-class MySSHServerSession(asyncssh.SSHServerSession):
-    def shell_requested(self):
-        return self
+    await process.redirect(stdin=bc_proc.stdin, stdout=bc_proc.stdout,
+                           stderr=bc_proc.stderr)
+    await process.stdout.drain()
+    process.exit(0)
 
-    def connection_made(self, chan):
-        self._chan = chan
-        self._chan.write('Hello, World\n')
-        self._chan.close()
+async def start_server() -> None:
+    await asyncssh.listen('', 8022, server_host_keys=['ssh_host_key'],
+                          authorized_client_keys='ssh_user_ca',
+                          process_factory=handle_client)
 
-async def start_server():
-    await asyncssh.create_server(
-        MySSHServer, '', 8022,
-        server_host_keys=['ssh_host_key']
-    )
+loop = asyncio.new_event_loop()
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(start_server())
+try:
+    loop.run_until_complete(start_server())
+except (OSError, asyncssh.Error) as exc:
+    sys.exit('Error starting server: ' + str(exc))
+
 loop.run_forever()
-
